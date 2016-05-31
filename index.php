a175ef8f9423f75928024b655abe760bd6b04361
germain009 <reasonres@gmail.com>
2016-05-31 09:15:05 +0200

    re

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $page='home';
 function __autoload($class_name) {
     require('models/'.$class_name.'.class.php');
 }
-require('config.php');
+require('apps/config.php');
 $link = mysqli_connect($localhost, $login, $pass, $database);
 if (!$link)
 {
