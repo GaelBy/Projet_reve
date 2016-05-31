e3db43eec7f7a6ca9916b3e5b33c95015667be44
christianHam <christian.hamada@gmail.com>
2016-05-31 10:30:26 +0200

    modif traitement avis

--- apps/traitement_avis.php
+++ apps/traitement_avis.php
@@ -7,40 +7,68 @@ if (!isset($_SESSION['login']))
 
 	if (isset($_SESSION['id'], $_POST['content'], $_POST['id_produit'], $_POST['note']))
 	{
-		$produit_manager = new ProduitManager($link);
-		$user_manager = new UserManager($link);
-		$avis_manager = new AvisManager($link);
-		$produit = $produit_manager->findById($_POST['id_produit']);
-		$user = $user_manager->findById($_SESSION['id']);
-		$avis = $avis_manager->create($_POST, $produit, $user);
-		//$date_avis = date('Y-m-d H:m:i');
-		//$query = "INSERT INTO avis (id_author, content, id_produit, note) VALUES ('".$id_author."', '".$content ."', '".$id_produit."', '".$note."')";
-		// $res = mysqli_query( $link, $query);
-		header('Location: ?page=produit&id='.$produit->getId());
-		exit;
-	}
+		if (isset($_GET['action']) && $_GET['action'] == "creer" )		
+		{
+			try
+			{
+			$produit_manager = new ProduitManager($link);
+			$user_manager = new UserManager($link);
+			$avis_manager = new AvisManager($link);
+			$produit = $produit_manager->getById($_POST['id_produit']);
+			$user = $user_manager->getById($_SESSION['id']);
+			$avis = $avis_manager->create($_POST, $produit, $user);
+			//$date_avis = date('Y-m-d H:m:i');
+			//$query = "INSERT INTO avis (id_author, content, id_produit, note) VALUES ('".$id_author."', '".$content ."', '".$id_produit."', '".$note."')";
+			// $res = mysqli_query( $link, $query);
+			header('Location: ?page=produit&id='.$produit->getId());
+			exit;
+			}
 
-	if (isset($_SESSION['id'], $_POST['content'], $_POST['id_produit'], $_POST['note']))
-	{
-		$produit_manager = new ProduitManager($link);
-		$user_manager = new UserManager($link);
-		$avis_manager = new AvisManager($link);
-		$produit = $produit_manager->findById($_POST['id_produit']);
-		$user = $user_manager->findById($_SESSION['id']);
-		$avis = $avis_manager->update($_POST, $produit, $user);
-		header('Location: ?page=produit&id='.$produit->getId());
-		exit;
-	}
+			catch (Exception $e);
+	    	{
+	    	$error = $e->getMessage;
+	    	}
+		}
+	
 
-	if (isset($_SESSION['id'], $_POST['content'], $_POST['id_produit'], $_POST['note']))
-	{
-		$produit_manager = new ProduitManager($link);
-		$user_manager = new UserManager($link);
-		$avis_manager = new AvisManager($link);
-		$produit = $produit_manager->findById($_POST['id_produit']);
-		$user = $user_manager->findById($_SESSION['id']);
-		$avis = $avis_manager->delete($_POST, $produit, $user);
-		header('Location: ?page=produit&id='.$produit->getId());
-		exit;
+		else if(isset($_GET['action']) && $_GET['action'] == "modifier")
+		{
+			try
+			{
+			$produit_manager = new ProduitManager($link);
+			$user_manager = new UserManager($link);
+			$avis_manager = new AvisManager($link);
+			$produit = $produit_manager->getById($_POST['id_produit']);
+			$user = $user_manager->getById($_SESSION['id']);
+			$avis = $avis_manager->update($_POST, $produit, $user);
+			header('Location: ?page=produit&id='.$produit->getId());
+			exit;
+			}
+
+			catch (Exception $e);
+	    	{
+	    	$error = $e->getMessage;
+	    	}
+
+		}
+
+		else if(isset($_GET['action']) && $_GET['action'] == "supprimer")
+		{
+			try
+			{
+			$produit_manager = new ProduitManager($link);
+			$user_manager = new UserManager($link);
+			$avis_manager = new AvisManager($link);
+			$produit = $produit_manager->getById($_POST['id_produit']);
+			$user = $user_manager->getById($_SESSION['id']);
+			$avis = $avis_manager->delete($_POST, $produit, $user);
+			header('Location: ?page=produit&id='.$produit->getId());
+			exit;
+			}
+			catch (Exception $e);
+		    {
+		    $error = $e->getMessage;
+		    }
 	}
+}
 ?>
