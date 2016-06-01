ca247bdf2b2d45cab8ff1d348a1facb46a6c7767
germain009 <reasonres@gmail.com>
2016-06-01 09:23:08 +0200

    re

--- apps/sub_category.php
+++ apps/sub_category.php
@@ -1,16 +1,16 @@
 <?php
 
-$res_sub = new SubCategoryManager($link);
+$res_sub = new ProduitManager($link);
 
 $_id_sub = 1;/*$_GET['id_category'];*/
 
  
 
 
-$array_sub = $res_sub->getByIdCategory($_id_sub);
+$array_sub = $res_sub->getBySubProduits($_id_sub);
 
 
-$iSub = 0;
+$iSub = 0 ;
 $nbSub = count( $array_sub );
 while($iSub < $nbSub){
 	$affiche_sub = $array_sub[$iSub];
