2ff51d0ef88fd4f0924777b8f9a5ae7e3b337408
GaelBy <g.benay@hotmail.fr>
2016-06-01 11:02:54 +0200

    produits et dépendances

--- apps/produit.php
+++ apps/produit.php
@@ -1,5 +1,16 @@
 <?php
-
-require('views/produit.phtml');
-
+if ($produit->getStatut())
+{
+	$manager = new ProduitsManager($link);
+	$produit = $manager->getById($_GET['id_produit']);
+	$id = $produit->getId();
+	$reference = $produit->getReference();
+	$stock = $produit->getStock();
+	$prix_uni_ht = $produit->getPrixUniHt();
+	$prix_uni_ttc = $produit->getPrixUniTtc();
+	$description = $produit->getDescription();
+	$image = $produit->getImage();
+	$nom = $produit->getNom();
+	require('views/produit.phtml');
+}
 ?>
