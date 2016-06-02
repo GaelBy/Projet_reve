5a7a7320c099abfe9e40ac83a50bcfcef17682b1
gmarbach <marbachweb@gmail.com>
2016-06-02 16:59:09 +0200

    boucle liste produits

--- apps/ls_stock.php
+++ apps/ls_stock.php
@@ -1,6 +1,12 @@
 <?php
+$count=0;
+$manager = new ProduitsManager ($link);
+$lsProduits = $manager->getAll();
 
-
-require('views/ls_stock.phtml');
-
+while($count<sizeof($lsProduits))
+{
+	$produit = $lsProduits[$count];
+	require('views/ls_stock.phtml');
+	$count++;
+}
 ?>
