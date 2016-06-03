92b2411b3fba945f380ac62e943d512b3d581a8e
GaelBy <g.benay@hotmail.fr>
2016-06-03 14:52:28 +0200

    premier jet admin_produits

--- apps/admin_produits.php
+++ apps/admin_produits.php
@@ -1,7 +1,17 @@
 <?php
 if (isset($_SESSION['admin']) && $_SESSION['admin'])
 {
-	
+	if (isset($_GET['action']) && $_GET['action'] == 'create')
+	{
+		$produit = new Produits($link);
+		$action = 'ajouter';
+	}
+	if (isset($_GET['action'], $_GET['id_produit']) && $_GET['action'] == 'edit')
+	{
+		$manager = new ProduitsManager($link);
+		$produit = $manager->getById($_GET['id_produit']);
+		$action = 'modifier';
+	}
 	require('views/admin_produits.phtml');
 }
 
