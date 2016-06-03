78a73b2e13a4d3cbeca28438e9af5012c507ab93
germain009 <reasonres@gmail.com>
2016-06-03 10:15:03 +0200

    re

--- apps/profil_user.php
+++ apps/profil_user.php
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-ls_panier.php
 
-=======
->>>>>>> 77480c144f14e58d9c2979e7aa698d7733b4c22f
-=======
->>>>>>> 6b621e790696d55bdfb77200f11f2e115514c6c0
 <?php
-if(isset($_SESSION['id']))
-{
+/*if(isset($_SESSION['id']))
+{*/
 	$manager = new UserManager($link);
-	$id= $_SESSION['id']; 
+	$id= 1 /*$_SESSION['id']*/; 
 	$user = $manager->getById($id);
-	require('apps/profil_user.php');
-}
+	require('views/profil_user.phtml');
+/*}**/
 ?>
