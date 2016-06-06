3580fd26ab178c2dc35585bbdcd0be25481ce41a
GaelBy <g.benay@hotmail.fr>
2016-06-06 17:03:30 +0200

    correction panier

--- models/Panier.class.php
+++ models/Panier.class.php
@@ -110,7 +110,7 @@ class Panier {
     if ($this->produits[$i]->getQuantite() === null)
       $this->produits[$i]->setQuantite(1);
     $this->nbre_produits = sizeof($this->produits); // le nombre de produits correspond à la taille de la liste
-    $this->prix = $this->prix + $produit->setPrixUniTtc();
+    $this->prix = $this->prix + $produit->updatePrixUniTtc();
     $this->poids = $this->poids + $produit->getPoidsUni();
     $manager = new ProduitsManager($this->link);
     $manager->initPanierProduit($produit);
