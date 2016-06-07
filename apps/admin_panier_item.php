662aea5210488199e264ff834cd69d80ecd326d4
GaelBy <g.benay@hotmail.fr>
2016-06-07 10:17:41 +0200

    liste des produits des paniers à valider

--- apps/admin_panier_item.php
+++ apps/admin_panier_item.php
@@ -1,5 +1,11 @@
 <?php
-
-require('views/admin_panier_item.phtml');
+$list = $panier->getProduits();
+$i = 0;
+while ($i < sizeof($list))
+{
+	$produit = $list[$i];
+	require('views/admin_panier_item.phtml');
+	$i++;
+}
 
 ?>
