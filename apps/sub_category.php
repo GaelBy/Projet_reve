e8c1516e61be7c9391019690a19f0cab15b03e09
germain009 <reasonres@gmail.com>
2016-06-07 12:00:23 +0200

    re

--- apps/sub_category.php
+++ apps/sub_category.php
@@ -1,11 +1,18 @@
+
+
+
+
 <?php
 if(isset($_GET['id_sub_category']))
 {
 	$manager = new SubCategoryManager($link);
 	$subcategory = $manager->getById($_GET['id_sub_category']);
+    
+	
 	require('views/sub_category.phtml');
+    
 }
-// require('apps/produit_item.php');
+    //require('apps/produit_item.php');
 
 
 ?>
