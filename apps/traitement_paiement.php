aef5a8ab1adc7ad6f63eca04509e30004bba56b5
GaelBy <g.benay@hotmail.fr>
2016-06-09 13:57:41 +0200

    debug

--- apps/traitement_paiement.php
+++ apps/traitement_paiement.php
@@ -7,17 +7,27 @@ if (isset($_POST['action']) && $_POST['action'] == 'adresse')
 		$user = $user_manager->getById($_SESSION['id']);
 		$adresse_manager = new AdresseManager($link);
 		$list_adresse = $user->getAdresse();
-		$i = 0;
-		while ($i < sizeof($list_adresse))
+		if (empty($list_adresse))
 		{
-			$adresse = $list_adresse[$i];
-			$i++;
-			$adresse->setNom($_POST['nom_adresse'.$i]);
-			$adresse->setNumero($_POST['numero'.$i]);
-			$adresse->setRue($_POST['rue'.$i]);
-			$adresse->setVille($_POST['ville'.$i]);
-			$adresse->setCodePostal($_POST['code_postal'.$i]);
-			$adresse = $adresse_manager->update($adresse);
+			$data1 = ['id_user'=>$user->getId(), 'nom_adresse' => $_POST['nom_adresse1'], 'numero' => $_POST['numero1'], 'rue' => $_POST['rue1'], 'ville' => $_POST['ville1'], 'code_postal' => $_POST['code_postal1'], 'type_adresse' => 'facturation'];
+			$data2 = ['id_user'=>$user->getId(), 'nom_adresse' => $_POST['nom_adresse2'], 'numero' => $_POST['numero2'], 'rue' => $_POST['rue2'], 'ville' => $_POST['ville2'], 'code_postal' => $_POST['code_postal2'], 'type_adresse' => 'livraison'];
+			$adresse1 = $adresse_manager->create($data1);
+			$adresse2 = $adresse_manager->create($data2);
+		}
+		else
+		{
+			$i = 0;
+			while ($i < sizeof($list_adresse))
+			{
+				$adresse = $list_adresse[$i];
+				$i++;
+				$adresse->setNom($_POST['nom_adresse'.$i]);
+				$adresse->setNumero($_POST['numero'.$i]);
+				$adresse->setRue($_POST['rue'.$i]);
+				$adresse->setVille($_POST['ville'.$i]);
+				$adresse->setCodePostal($_POST['code_postal'.$i]);
+				$adresse = $adresse_manager->update($adresse);
+			}
 		}
 	}
 	catch (Exception $e)
